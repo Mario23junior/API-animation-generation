9e015016218686ae55e2cfcc4a1242eff9647347
Mario23junior <mariojunior3251@gmail.com>
2021-02-07 03:11:34 -0300

    Criando entidade de descricao da animatio

---
+++ src/main/java/com/project/animation/Model/Animation.java
@@ -0,0 +1,48 @@
+package com.project.animation.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Animation {
+    
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String TituloOriginal;
+	private String titulo;
+	private String descricao;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTituloOriginal() {
+		return TituloOriginal;
+	}
+	public void setTituloOriginal(String tituloOriginal) {
+		TituloOriginal = tituloOriginal;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+	
+	
+	
+}
