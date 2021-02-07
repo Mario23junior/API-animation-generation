bf7ae20886e361642f3f45d6438eb40d91a7a124
Mario23junior <mariojunior3251@gmail.com>
2021-02-06 23:59:54 -0300

    Iniciando projeto

---
+++ src/main/java/com/project/animation/AnimationApplication.java
@@ -0,0 +1,13 @@
+package com.project.animation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimationApplication.class, args);
+	}
+
+}
