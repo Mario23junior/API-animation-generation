022dbc5a7567a30f4d27358650840d882ce62cc5
Mario23junior <mariojunior3251@gmail.com>
2021-02-08 03:17:53 -0300

    Listando animation apartir do id fornecido

--- src/main/java/com/project/animation/Controller/AnimationController.java
+++ src/main/java/com/project/animation/Controller/AnimationController.java
@@ -1,11 +1,14 @@
 package com.project.animation.Controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.project.animation.Model.Animation;
 import com.project.animation.Repository.AnimationRepository;
@@ -24,6 +27,13 @@ public class AnimationController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Animation salvarAnimation(@RequestBody Animation animation) {
 		return animationRepository.save(animation);
+	}
+	
+	@GetMapping("/{id}")
+	public Animation obterId(@PathVariable Integer id) {
+		return animationRepository
+			      .findById(id)
+			      .orElseThrow( () ->  new ResponseStatusException(HttpStatus.NO_CONTENT,"Desculpe animation não encontrada"));
 	}
 	
 }
