9ecaf1cdaccfc8eb16106248d219e1b439133910
Mario23junior <mariojunior3251@gmail.com>
2021-02-08 02:35:15 -0300

    Alterando model de acesso ao repositorys

--- src/main/java/com/project/animation/Repository/SinopsInfoRepository.java
+++ src/main/java/com/project/animation/Repository/SinopsInfoRepository.java
@@ -3,7 +3,9 @@ package com.project.animation.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.project.animation.Model.SinopsInfo;
+
 @Repository
-public interface SinopsInfoRepository extends JpaRepository<SinopsInfoRepository, Integer>{
+public interface SinopsInfoRepository extends JpaRepository<SinopsInfo, Integer>{
 
 }
