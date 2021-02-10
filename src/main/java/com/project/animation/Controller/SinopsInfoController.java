c4a52234725f595300f1f0d7b3ff53baaef31e84
Mario23junior <mariojunior3251@gmail.com>
2021-02-10 02:46:32 -0300

    Atualizando informacoes de sinops

--- src/main/java/com/project/animation/Controller/SinopsInfoController.java
+++ src/main/java/com/project/animation/Controller/SinopsInfoController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -48,4 +49,17 @@ public class SinopsInfoController {
 	                	   return delSino;
 	                   }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT,"Sinopse não encontrado"));
 	}
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void UpdateSinops(@PathVariable Integer id, @RequestBody SinopsInfo sinoInfo) {
+		    sinopsInfoRepository
+		                    .findById(id)
+		                    .map(update -> {
+		                    	sinoInfo.setId(update.getId());
+		                    	sinopsInfoRepository.save(sinoInfo);
+		                    	return update;
+		                    }).orElseThrow( () -> new ResponseStatusException(HttpStatus.NO_CONTENT,"Sinops não encontrada"));
+	}
+	
 }
