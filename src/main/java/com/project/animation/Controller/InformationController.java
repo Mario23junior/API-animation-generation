3be6f1e0129a758405f9e00a4ac7af3d1ea03c2c
Mario23junior <mariojunior3251@gmail.com>
2021-02-10 04:51:59 -0300

    Listando dados por id

--- src/main/java/com/project/animation/Controller/InformationController.java
+++ src/main/java/com/project/animation/Controller/InformationController.java
@@ -1,11 +1,14 @@
 package com.project.animation.Controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.project.animation.Model.Information;
 import com.project.animation.Repository.InformationRepository;
@@ -25,6 +28,14 @@ public class InformationController {
 	public Information salvarDados(@RequestBody Information information) {
 		return informationRepository.save(information);
 	}
+	
+	@GetMapping("/{id}")
+	public Information listarPorId(@PathVariable Integer id) {
+		return informationRepository
+				           .findById(id)
+				           .orElseThrow( () -> new ResponseStatusException(HttpStatus.NO_CONTENT,"Dados não encontrados"));
+	}
+ 	
 	
 	
 	
