2688b6a1aac2c231e29a070a61e582966bba0a86
Mario23junior <mariojunior3251@gmail.com>
2021-02-10 01:58:34 -0300

    Listando informacoes de sinops por id

--- src/main/java/com/project/animation/Controller/SinopsInfoController.java
+++ src/main/java/com/project/animation/Controller/SinopsInfoController.java
@@ -1,11 +1,14 @@
 package com.project.animation.Controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.project.animation.Model.SinopsInfo;
 import com.project.animation.Repository.SinopsInfoRepository;
@@ -24,6 +27,14 @@ public class SinopsInfoController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public SinopsInfo salvarSinosInfo(@RequestBody SinopsInfo sinopsInfo) {
 		return sinopsInfoRepository.save(sinopsInfo);
+	}
+	
+	@GetMapping("/{id}")
+	public SinopsInfo obterId(@PathVariable Integer id) {
+		return sinopsInfoRepository
+				       .findById(id)
+				       .orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT,"Sinops não encontrada"));
+		
 	}
 	
 }
