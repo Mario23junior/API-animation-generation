98264f92271f85335acaf48b73505db26d393385
Mario23junior <mariojunior3251@gmail.com>
2021-02-11 04:02:05 -0300

    Criando modelo DTO de SinopsInfoDTO

---
+++ src/main/java/com/project/animation/DTO/SinopseInfoDTO.java
@@ -0,0 +1,43 @@
+package com.project.animation.DTO;
+
+public class SinopseInfoDTO {
+     
+	private Integer id;
+	private String DataDeCriacao;
+	private String situacao;
+	private boolean filme;
+	private boolean serie;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDataDeCriacao() {
+		return DataDeCriacao;
+	}
+	public void setDataDeCriacao(String dataDeCriacao) {
+		DataDeCriacao = dataDeCriacao;
+	}
+	public String getSituacao() {
+		return situacao;
+	}
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+	public boolean isFilme() {
+		return filme;
+	}
+	public void setFilme(boolean filme) {
+		this.filme = filme;
+	}
+	public boolean isSerie() {
+		return serie;
+	}
+	public void setSerie(boolean serie) {
+		this.serie = serie;
+	}
+	
+	
+}
