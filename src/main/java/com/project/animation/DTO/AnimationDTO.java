e07b121799ce2c05016f63a3cc6b841ea2089358
Mario23junior <mariojunior3251@gmail.com>
2021-02-11 03:49:25 -0300

    Criando modelo DTO de Animation

--- src/main/java/com/project/animation/DTO/AnimationDTO.java
+++ src/main/java/com/project/animation/DTO/AnimationDTO.java
@@ -1,5 +1,36 @@
 package com.project.animation.DTO;
 
 public class AnimationDTO {
-
+    
+	private Integer id;
+	private String titulo;
+	private String TituloOriginal;
+	private String descricao;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTituloOriginal() {
+		return TituloOriginal;
+	}
+	public void setTituloOriginal(String tituloOriginal) {
+		TituloOriginal = tituloOriginal;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
 }
