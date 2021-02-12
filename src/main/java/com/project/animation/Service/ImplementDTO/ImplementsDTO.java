6ede08985fd5c1df575af16edd4c0ec6d9437000
Mario23junior <mariojunior3251@gmail.com>
2021-02-12 02:49:06 -0300

    Criando instancia de repository Animation

--- src/main/java/com/project/animation/Service/ImplementDTO/ImplementsDTO.java
+++ src/main/java/com/project/animation/Service/ImplementDTO/ImplementsDTO.java
@@ -2,11 +2,21 @@ package com.project.animation.Service.ImplementDTO;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.project.animation.Model.Animation;
+import com.project.animation.Repository.AnimationRepository;
 import com.project.animation.Service.ServiceDTOImplemet;
 
+@Service
 public class ImplementsDTO implements ServiceDTOImplemet{
-
+   
+	private AnimationRepository animationRepository;
+	
+	public ImplementsDTO( AnimationRepository animationRepository) {
+		this.animationRepository = animationRepository;
+	}
+	
 	@Override
 	public List<Animation> ListAll() {
 		return null;
