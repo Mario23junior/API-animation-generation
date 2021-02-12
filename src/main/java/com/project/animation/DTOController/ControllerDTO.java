19f6f45f7cb8e00afb4b86d1bc39101039924043
Mario23junior <mariojunior3251@gmail.com>
2021-02-12 03:54:51 -0300

    Criando endpoint de para listar todos os dados necessarios da camda de DAO

--- src/main/java/com/project/animation/DTOController/ControllerDTO.java
+++ src/main/java/com/project/animation/DTOController/ControllerDTO.java
@@ -1,5 +1,25 @@
 package com.project.animation.DTOController;
 
-public class ControllerDTO {
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.animation.DTO.AnimationDTO;
+import com.project.animation.Service.ServiceDTOImplemet;
+
+@RestController
+public class ControllerDTO {
+    
+	private ServiceDTOImplemet mapService;
+	
+	public ControllerDTO( ServiceDTOImplemet mapService) {
+		this.mapService = mapService;
+	}
+	
+	@GetMapping("/listAll")
+	public List<AnimationDTO> ListAllData() {
+		List<AnimationDTO> animationDAO = mapService.ListAll();
+		return animationDAO;
+	}
 }
